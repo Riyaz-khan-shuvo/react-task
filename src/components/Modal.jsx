a55778c703929bfa78a7f9fd96fe4f2c0de839f7
Riyaz-khan-shuvo <mdriyaz5965@gmail.com>
2023-12-23 13:51:53 +0600

    problem - 2 has completed

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,36 +1,77 @@
-import React from 'react';
 
-const Modal = ({ title, color, onClose, children, footerContent, onSearch }) => {
+import React, { useEffect, useState } from 'react';
+
+const Modal = ({ showModal, title, onClose, footerContent, onSearch, contactData, id, setSearchText }) => {
+
+
+  const handleInputChange = (e) => {
+    const value = e.target.value;
+    setInputValue(value);
+    setSearchText(value); // Update the state in the parent component
+  };
+
+
   return (
-    <div className="modal" tabIndex="-1" role="dialog">
-      <div className="modal-dialog" role="document">
-        <div className="modal-content" style={{ borderColor: color }}>
-          <div className="modal-header">
-            <h5 className="modal-title" style={{ color }}>
-              {title}
-            </h5>
-            <button type="button" className="btn-close" onClick={onClose}></button>
-          </div>
-          <div className="modal-body">
-            <div className="mb-3">
-              <input
-                type="text"
-                className="form-control"
-                placeholder="Search..."
-                onChange={(e) => onSearch(e.target.value)}
-              />
+    <>
+      {showModal && (
+        <div className="modal fade" id={`${id}`} tabIndex="-1" aria-labelledby={`${id}Label`} aria-hidden="true">
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title" id={`${id}Label`}> {title} </h5>
+
+                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div className="modal-body">
+                <div className="d-flex justify-content-center gap-3">
+                  <button
+                    className="btn"
+                    style={{ background: `#46139f`, color: "#fff" }}
+                    type="button"
+                    // onClick={handleModalAClick}
+                    data-bs-toggle="modal"
+                    data-bs-target="#allContact"
+                  >
+                    All Contacts
+                  </button>
+                  <button
+                    className="btn"
+                    type="button"
+                    data-bs-toggle="modal"
+                    data-bs-target="#usContact"
+                    style={{ background: `#ff7f50`, color: "#fff" }}
+                  // onClick={handleModalBClick}
+                  >
+                    US Contacts
+                  </button>
+                </div>
+                <div>
+                  <div>
+                    <div className="m-3">
+                      <input type="text" onChange={e => setSearchText(e.target.value)} className="form-control" id="formGroupExampleInput" placeholder="Search Data" />
+                    </div>
+                  </div>
+                  <div>
+                    {
+                      contactData.map((contact, index) => <div className='m-3' key={index}>
+                        <div className='border p-3'>
+                          Name: {contact.phone}<br />
+                          {contact.country.name}
+                        </div>
+                      </div>)
+                    }
+                  </div>
+                </div>
+              </div>
+              <div className="modal-footer">
+                {footerContent}
+                <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+              </div>
             </div>
-            {children}
-          </div>
-          <div className="modal-footer">
-            {footerContent}
-            <button type="button" className="btn btn-secondary" onClick={onClose}>
-              Close
-            </button>
           </div>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
--- src/components/Problem-2.jsx
+++ src/components/Problem-2.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Modal from './Modal'; // You need to implement a Modal component
-
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // Make sure to include the Bootstrap JavaScript bundle
 const Problem2 = () => {
     const [showModalA, setShowModalA] = useState(false);
     const [showModalB, setShowModalB] = useState(false);
@@ -9,17 +9,35 @@ const Problem2 = () => {
     const [contacts, setContacts] = useState([]);
     const [searchTerm, setSearchTerm] = useState('');
     const [page, setPage] = useState(1);
+    const [USA, setUSA] = useState([]);
+    const [searchText, setSearchText] = useState('');
+
 
     useEffect(() => {
-        // Fetch initial data from the API
         fetchContacts();
-    }, []);
+        fetchUSA();
+    }, [searchText]);
 
     const fetchContacts = async () => {
         try {
-            const response = await fetch(`https://contact.mediusware.com/api/contacts/?page=${page}`);
+            let apiUrlWithParams = `https://contact.mediusware.com/api/contacts/`
+            if (searchText) {
+                apiUrlWithParams += `?search=${searchText}&page_size=1`;
+            }
+            console.log(apiUrlWithParams);
+            const response = await fetch(apiUrlWithParams);
+
             const data = await response.json();
-            setContacts((prevContacts) => [...prevContacts, ...data.results]);
+            setContacts(data.results);
+        } catch (error) {
+            console.error('Error fetching contacts:', error);
+        }
+    };
+    const fetchUSA = async () => {
+        try {
+            const response = await fetch(`https://contact.mediusware.com/api/country-contacts/United%20States/`);
+            const data = await response.json();
+            setUSA(data.results);
         } catch (error) {
             console.error('Error fetching contacts:', error);
         }
@@ -61,21 +79,9 @@ const Problem2 = () => {
         setShowModalA(true);
         setShowModalB(false);
         setShowModalC(false);
-        // Change the URL here
     };
 
-    const handleModalBClick = () => {
-        setShowModalA(false);
-        setShowModalB(true);
-        setShowModalC(false);
-        // Change the URL here
-    };
 
-    const handleModalCClick = () => {
-        setShowModalA(false);
-        setShowModalB(false);
-        setShowModalC(true);
-    };
 
     const closeModal = () => {
         setShowModalA(false);
@@ -83,121 +89,89 @@ const Problem2 = () => {
         setShowModalC(false);
     };
 
+
+    console.log(contacts);
+
+
+
+
+
     return (
-        //         <div className="container">
-        //             <div className="row justify-content-center mt-5">
-        //                 <h4 className="text-center text-uppercase mb-5">Problem-2</h4>
+        <div className="container">
+            <div className="row justify-content-center mt-5">
+                <h4 className="text-center text-uppercase mb-5">Problem-2</h4>
 
-        //                 <div className="d-flex justify-content-center gap-3">
-        //                     <button
-        //                         className="btn btn-lg btn-outline-primary"
-        //                         type="button"
-        //                         onClick={handleModalAClick}
-        //                     >
-        //                         All Contacts
-        //                     </button>
-        //                     <button
-        //                         className="btn btn-lg btn-outline-warning"
-        //                         type="button"
-        //                         onClick={handleModalBClick}
-        //                     >
-        //                         US Contacts
-        //                     </button>
-        //                 </div>
-
-        //                 {showModalA && (
-        //                     <Modal
-        //                         title="Modal A"
-        //                         color="#46139f"
-        //                         onClose={closeModal}
-        //                         footerContent={
-        //                             <div>
-        //                                 <input
-        //                                     type="checkbox"
-        //                                     id="onlyEvenCheckbox"
-        //                                     checked={onlyEven}
-        //                                     onChange={() => setOnlyEven(!onlyEven)}
-        //                                 />
-        //                                 <label htmlFor="onlyEvenCheckbox">Only even</label>
-        //                             </div>
-        //                         }
-        //                         onSearch={(value) => setSearchTerm(value)}
-        //                     >
-        //                         {/* Display contacts from API in Modal A */}
-        //                         {filterContacts().map((contact) => (
-        //                             <div key={contact.id} onClick={handleModalCClick}>
-        //                                 <div key={contact.id}>
-        //                                     {contact.name} - {contact.country}
-        //                                 </div>
-        //                             </div>
-        //                         ))}
-        //                     </Modal>
-        //                 )}
-
-        //                 {showModalB && (
-        //                     <Modal
-        //                         title="Modal B"
-        //                         color="#ff7f50"
-        //                         onClose={closeModal}
-        //                         footerContent={
-        //                             <div>
-        //                                 <input
-        //                                     type="checkbox"
-        //                                     id="onlyEvenCheckbox"
-        //                                     checked={onlyEven}
-        //                                     onChange={() => setOnlyEven(!onlyEven)}
-        //                                 />
-        //                                 <label htmlFor="onlyEvenCheckbox">Only even</label>
-        //                             </div>
-        //                         }
-        //                         onSearch={(value) => setSearchTerm(value)}
-        //                     >
-        //                         {/* Display contacts from API in Modal B (only from US) */}
-        //                         {filterContacts().map((contact) => (
-        //   <div key={contact.id} onClick={handleModalCClick}>
-        //     <div key={contact.id}>
-        //       {contact.name} - {contact.country}
-        //     </div>
-        //   </div>
-        // ))}
-        //                     </Modal>
-        //                 )}
-
-        //                 {showModalC && (
-        //                     <Modal title="Modal C" color="#46139f" onClose={closeModal}>
-        //                         {/* Display contact details in Modal C */}
-        //                         {/* You need to implement the content for Modal C based on the selected contact */}
-        //                     </Modal>
-        //                 )}
-
-
-        //             </div>
-        //         </div>
-        <>
-            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
-                Launch demo modal
-            </button>
-
-            <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div class="modal-dialog" role="document">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
-                        </div>
-                        <div class="modal-body">
-                            ...
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                            <button type="button" class="btn btn-primary">Save changes</button>
-                        </div>
-                    </div>
+                <div className="d-flex justify-content-center gap-3">
+                    <button
+                        className="btn btn-lg btn-outline-primary"
+                        type="button"
+                        onClick={handleModalAClick}
+                        data-bs-toggle="modal"
+                        data-bs-target="#allContact"
+                    >
+                        All Contacts
+                    </button>
+                    <button
+                        className="btn btn-lg btn-outline-warning"
+                        type="button"
+                        onClick={handleModalAClick}
+                        data-bs-toggle="modal"
+                        data-bs-target="#usContact"
+                    >
+                        US Contacts
+                    </button>
                 </div>
+
+                <Modal
+                    title="All Contact"
+                    id="allContact"
+                    onClose={closeModal}
+                    footerContent={
+                        <div>
+                            <input
+                                type="checkbox"
+                                id="onlyEvenCheckbox"
+                                checked={onlyEven}
+                                onChange={() => setOnlyEven(!onlyEven)}
+                            />
+                            <label className='ms-3' htmlFor="onlyEvenCheckbox">Only even</label>
+                        </div>
+                    }
+                    contactData={filterContacts().map((contact) => contact)}
+                    onSearch={(value) => setSearchTerm(value)}
+                    showModal={true}
+                    setSearchText={setSearchText}
+                />
+
+                <Modal
+                    title="US Contact"
+                    id="usContact"
+                    onClose={closeModal}
+                    footerContent={
+                        <div>
+                            <input
+                                type="checkbox"
+                                id="onlyEvenCheckbox"
+                                checked={onlyEven}
+                                onChange={() => setOnlyEven(!onlyEven)}
+                            />
+                            <label className='ms-3' htmlFor="onlyEvenCheckbox">Only even</label>
+                        </div>
+                    }
+                    contactData={USA.map((contact) => contact)}
+                    onSearch={(value) => setSearchTerm(value)}
+                    showModal={true}
+                    setSearchText={setSearchText}
+                />
+
+
             </div>
-        </>
+        </div>
+
+
+
+
+
     );
 };
 
